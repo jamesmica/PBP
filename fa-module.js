0c938862fbc592ea600ec6c24298c5e40fd5f909
jamesmica <123773695+jamesmica@users.noreply.github.com>
2024-04-10 12:25:51 +0200

    deleted useless files

--- fa-module.js
+++
@@ -1,6 +0,0 @@
-import { library, dom } from '@fortawesome/fontawesome-svg-core';
-import { faHouse, faBook, faCompass, faCirclePlus } from '@fortawesome/free-solid-svg-icons';
-library.add(faHouse, faBook, faCompass, faCirclePlus);
-
-// Remplace automatiquement les icônes <i> par les SVG
-dom.watch();
