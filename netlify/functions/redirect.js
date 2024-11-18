3c5e7a43b4b1dcb9fa4085fbb8e65ef6fcf6a8ca
jamesmica <123773695+jamesmica@users.noreply.github.com>
2024-11-18 16:16:47 +0100

    Create redirect.js

---
+++ netlify/functions/redirect.js
@@ -0,0 +1,11 @@
+exports.handler = async (event, context) => {
+    // URL de destination sans paramètres
+    const redirectTo = "https://www.ithea-conseil.fr/bonnes-pratiques";
+
+    return {
+        statusCode: 301,
+        headers: {
+            Location: redirectTo
+        }
+    };
+};
